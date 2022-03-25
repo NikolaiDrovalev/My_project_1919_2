1c881c76f8df7ec01b673dc916061216ed87ce88
Nikolai <ndrovalev@gmail.com>
2022-03-25 16:24:18 +0300

    Урок 1. Добавил кнопку и повесил на нее ClickListener, по клику выводится Toast.

--- app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
@@ -2,11 +2,18 @@ package ru.geekbrains.myproject1919_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 import ru.geekbrains.myproject1919_2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<MaterialButton>(R.id.btn)
+        button.setOnClickListener{
+            Toast.makeText(this, "Нажали на кнопку", Toast.LENGTH_SHORT).show()
+        }
     }
 }
