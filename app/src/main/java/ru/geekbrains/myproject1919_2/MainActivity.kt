b0f1c0223f63fe30dabcb654dca3ae4de77291ce
Nikolai <ndrovalev@gmail.com>
2022-03-25 13:34:05 +0300

    Урок 1. Перевёл MainActivity на Kotlin.

--- app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
@@ -1,14 +1,12 @@
-package ru.geekbrains.myproject1919_2;
+package ru.geekbrains.myproject1919_2
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.myproject1919_2.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
