209499ab500060d8828a5317fbce9aae04932fc1
Nikolai <ndrovalev@gmail.com>
2022-03-27 22:39:31 +0300

    Урок 1. Добавил циклы for, forEach, repeat и вывел результаты в консоль.

--- app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.geekbrains.myproject1919_2
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,11 +21,28 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Нажали на кнопку", Toast.LENGTH_SHORT).show()
 
             val card = Card(city = "", 0)
+            card.copy(
+                city = cityName.setText("Казань").toString(),
+                temperature = cityTemp.setText("32").hashCode()
+            )
 
-            card.copy(city = cityName.setText("Казань").toString(), temperature = cityTemp.setText("32").hashCode())
+        }
 
+        for (i in 1..3){
+            Log.d("logs", "$i Kotlin")
+        }
+
+        val list = listOf(1,2,3,4,5)
+        list.forEach{
+            Log.d("logs","$it I love Kotlin")
+        }
+
+        repeat(10){
+            Log.d("logs", "$it Hello Kotlin")
         }
     }
 }
 
+
 data class Card(val city: String, val temperature: Int)
+
