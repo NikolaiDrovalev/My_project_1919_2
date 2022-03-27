eff228e643522d42dd46a9c6c8c2f5c1f6472da8
Nikolai <ndrovalev@gmail.com>
2022-03-27 20:13:39 +0300

    Урок 1. Добавил data class Card с двумя свойствами и вывел на экран.

--- app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myproject1919_2/MainActivity.kt
@@ -1,19 +1,24 @@
 package ru.geekbrains.myproject1919_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
-import ru.geekbrains.myproject1919_2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cityName = findViewById<TextView>(R.id.city)
+        val cityTemp = findViewById<TextView>(R.id.temperature)
+        Card(cityName.setText("Москва").toString(),cityTemp.setText("25").hashCode())
         val button = findViewById<MaterialButton>(R.id.btn)
-        button.setOnClickListener{
+        button.setOnClickListener {
             Toast.makeText(this, "Нажали на кнопку", Toast.LENGTH_SHORT).show()
         }
     }
 }
+
+data class Card(val city: String, val temperature: Int)
+
